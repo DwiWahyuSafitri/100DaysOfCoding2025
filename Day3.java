608ae1a11411aaee1779f65bc0415efce725348f
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-10 11:01:24 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class Day3 {
+    public static void main(String[] args) {
+        // ini adalah komentar 1 baris//
+        
+        /*ini adalah komentar lebih dari 1 baris
+        jika ingin memberi banyak komentar
+        */
+    }
+    
+}
