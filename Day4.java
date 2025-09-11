6c365666ea48b1a559baac14df8933874066b056
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-11 21:20:20 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,34 @@
+package pkg100dayscoding;
+
+
+public class Day4 {
+    public static void main(String[] args) {
+        //Pengunaan Escape sequence//
+        
+        // \n penggunaan baris baru //
+        System.out.println("MyNameIS\n Fitri");
+        
+        // \t penggunaan tab(spasi lebar)//
+        System.out.println("MyNameIs\t Fitri");
+        
+        // \b penggunaan backspace//
+        System.out.println("MyNameIs\b Fitri");
+        
+        // \r penggunaan kursor ke awal baris//
+        System.out.println("MyNameIs\r Fitri");
+        
+        // \f biasanya di pakai ganti halaman pada printer//
+        System.out.println("Page4\f Page5");
+        
+        // \' penggunaan tanda kutip tunggal//
+        System.out.println("MyNameIs\' Fitri '");
+        
+        // \" penggunaan tanda kutip ganda//
+        System.out.println("MyNameIs\" Fitri");
+        
+        // \\ backslash //
+        System.out.println("MyNameIs\\ Fitri");
+        
+    }
+    
+}
