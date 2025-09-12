9f6b042bd29dc1d51779e031a3a5abfc2811eff8
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-12 14:59:28 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+package pkg100dayscoding;
+
+public class Day5 {
+    public static void main(String[] args) {
+        byte q = 2;
+        short w = 20;
+        int e = 200;
+        long r = 2000;
+        
+        System.out.println(q);
+        System.out.println(w);
+        System.out.println(e);
+        System.out.println(r);
+    }
+}
