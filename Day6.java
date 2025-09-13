55ffae3d50dd4bf949533788cfb59d796b5dca41
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-13 10:19:47 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class day6 {
+    public static void main(String[] args) {
+        double a = 20.6;
+        float b = 31.1f;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
