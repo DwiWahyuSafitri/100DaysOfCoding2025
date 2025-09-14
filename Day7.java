2426a261d516bbf04bd1a1b28c3955ddedb3b7c2
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-14 11:08:33 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding;
+public class Day7 {
+    public static void main(String[] args) {
+        char GolDarah = 'o';
+        boolean mengerti = true;
+        
+        System.out.println(GolDarah);
+        System.out.println(mengerti);
+        
+              
+    }
+    
+}
