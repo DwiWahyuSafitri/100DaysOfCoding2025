eaab11d7542dcf0d924bf6cf44a34985189e8dea
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-15 16:13:01 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day8 {
+    public static void main(String[] args) {
+    String a = "Perkenalkan nama saya Dwi wahyu safitri";
+    String b = "Biasanya di pangil fitri";
+    String c = "Umur saya 18 tahun";
+    
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
