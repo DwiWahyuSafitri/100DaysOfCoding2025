46fc3c1f08e2fbde5bb04114960b2a01aead448a
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-16 22:06:25 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,16 @@
+package pkg100dayscoding;
+public class Day9 {
+    public static void main(String[] args) {
+        //deklarasi//
+        String warna;
+        
+        //inisialisasi variable//
+        warna = "pink";
+        System.out.println("warna favorit saya " + warna);
+        
+        //update nilai variiable//
+        warna = "biru";
+        System.out.println("saya mengganti warna favorit saya menjadi "+ warna);
+                
+    }
+}
