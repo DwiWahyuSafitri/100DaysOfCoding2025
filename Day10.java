bb29781a39dbf2301daeea424aad63ba730b2876
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-17 19:18:03 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,15 @@
+package pkg100dayscoding;
+public class Day10 {
+    public static void main(String[] args) {
+        final double LS=0.5; //satu kali buat bisa di pakai berkali kali//
+        double luas1= (LS * 3 * 4);
+        double luas2=(LS * 6 * 6);
+        
+        System.out.println("luas segitiga 1 yaitu : "+luas1);
+        System.out.println("luas segitiga 2 yaitu : "+luas2);
+        /*jadi konstanta itu di gunakan supaya nilai penting dalam program tetap sama
+        dan gampang di perbaiki kalau ada yang mau di ubah
+        */
+    }
+    
+}
