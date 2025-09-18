8fb2e1cc96455756367d33e96270ef0cd180c3ce
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-18 15:31:11 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,18 @@
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("masukkan nama");
+        String nama = Sc.nextLine();
+        
+        System.out.println("masukkan umur");
+        int umur = Sc.nextInt();
+        
+        System.out.println("masukkan tinggi badan");
+        double tinggi = Sc.nextDouble();
+         
+    }
+}
