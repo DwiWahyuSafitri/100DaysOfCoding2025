4a3abb8dd17eecaafded80fe806035b577cccb0b
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-22 10:59:10 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+package pkg100dayscoding;
+public class Day15 {
+    public static void main(String[] args) {
+        //penjumlahan//
+        int a = 1;
+        int b = 3;
+        int c = a+b;
+        System.out.println(c);
+        
+        //pengurangan//    
+        int d = 8;
+        int e = 4;
+        int f = d-e;
+        System.out.println(f);
+    }
+}
