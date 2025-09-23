88e8b04077e83e7c234465b7a5108326b20eca57
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-23 20:37:28 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,17 @@
+package pkg100dayscoding;
+public class Day16 {
+    public static void main(String[] args) {
+        //perkalian//
+        int a = 9;
+        int b = 9;
+        int hasilKali= a*b;
+        System.out.println(hasilKali);
+        
+        //pembagian//
+        int j = 10;
+        int k = 5;
+        int HasilBagi= j/k;
+        System.out.println(HasilBagi);
+    }
+    
+}
