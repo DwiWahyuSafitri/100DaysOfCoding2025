845236e065bfb86e27c805e548d1d6710714efa2
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-24 20:01:01 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding;
+public class Day17 {
+    public static void main(String[] args) {
+        int angka1 = 4;
+        int angka2 = 3;
+        int sisaBagi = angka1 % angka2;
+        
+        System.out.println(sisaBagi);
+    }
+    
+}
