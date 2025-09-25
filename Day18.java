66c801dc114b53ff2697bef48238ab8797cb315c
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-25 22:17:59 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        byte a = 110;
+        int LebihBsr = a;
+        System.out.println(a);
+        System.out.println(LebihBsr);
+    }
+}
