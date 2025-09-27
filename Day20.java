949382fd495d462a8edfe30c11405fffbb032645
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-27 20:43:35 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding;
+public class Day20 {
+    public static void main(String[] args) {
+        int tahun = 2006;
+        String a = String.valueOf(tahun);
+        
+        System.out.println(a);
+    }
+    
+}
