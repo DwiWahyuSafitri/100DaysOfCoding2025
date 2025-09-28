58e31b9e15cee23244d25e6c68bd264f34f411b3
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-09-28 22:41:56 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,19 @@
+package pkg100dayscoding;
+
+public class Day21 {
+
+    public static void main(String[] args) {
+        int a=8;
+        int b=4;
+        
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+
+    }
+
+}
