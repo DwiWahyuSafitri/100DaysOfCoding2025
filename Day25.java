e555cb06a2900a3c1d6ae58e4c88c2f4f4ecfe87
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-02 23:35:31 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,16 @@
+package pkg100dayscoding;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        int a = 7;
+        --a;
+        System.out.println(a);
+        
+        int b = 8;
+        ++b;
+        System.out.println(b);
+
+    }
+
+}
