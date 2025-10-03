35d5a4ab8901b99bb7ac22626e24f590fbfe8c46
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-03 21:42:26 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 8;
+        a+=1;
+        a-=1;
+        a/=4;
+        a*=2;
+        a%=2;
+        System.out.println(a);
+    }
+    
+}
