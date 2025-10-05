ecf4685ad3b2f8cbaf0d91374b25694ceff2c7c2
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-05 22:10:58 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding;
+
+public class Day28 {
+
+    public static void main(String[] args) {
+        int nilai1 = 10;
+        int nilai2 = 10;
+
+        System.out.println(nilai1 == nilai2);
+        System.out.println(nilai1 != nilai2);
+    }
+
+}
