36e46dbc823d3c4fccd4faba3f8fb8fa45354261
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-06 19:55:40 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        System.out.println(a < b);
+        System.out.println(a > b);
+    }
+
+}
