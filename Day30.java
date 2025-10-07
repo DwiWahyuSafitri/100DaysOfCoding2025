c93bb612ef40ddee3b2f151d1d55802a9e5f74ac
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-07 20:06:48 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class Day30 {
+
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 8;
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+    }
+
+}
