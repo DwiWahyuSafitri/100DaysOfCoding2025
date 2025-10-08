74463bebdc8ae2798627dd71af26cb1b7d037e35
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-08 22:22:36 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding;
+
+public class Day31 {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 5;
+
+        System.out.println(a < 10 && b > 3);
+        System.out.println(a > 10 && b > 3);
+    }
+
+}
