0b5c16ce0c0955afdea3604375e8854cb6078163
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-09 22:22:21 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class Day32 {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+
+        System.out.println(a > 2 || b < 2);
+    }
+
+}
