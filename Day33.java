8d02f9ca6a528c863f124d0b573a6b717ce7758d
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-10 21:23:17 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class Day33 {
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 11;
+
+        System.out.println(!(a == b));
+    }
+
+}
