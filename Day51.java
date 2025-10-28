c347f9a51563e1680eb63a5c940e3e144278cd70
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-28 23:17:09 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding;
+
+public class Day51 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i +". saya pasti bisa");
+        }
+    }
+    
+}
