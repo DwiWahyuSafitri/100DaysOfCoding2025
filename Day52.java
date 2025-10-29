bbc9b4248b550ef7e1275e1a11225260172f6d49
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-10-29 22:38:27 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day52 {
+    public static void main(String[] args) {
+        int a =5;
+        
+        while(a <= 6){
+            System.out.println("aku lulus DDP");
+            a++;
+        }
+            
+    }
+    
+}
