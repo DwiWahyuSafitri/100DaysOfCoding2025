23be060c70addbabcde9d044f728a329af1c581e
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-01 22:13:09 +0800

    Create Day55.java

---
+++ Day55.java
@@ -0,0 +1,17 @@
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Day55 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int angka = in.nextInt();
+        
+        if (angka > 70) {
+            System.out.println("Ujian Kroco Ji Pale");
+        }else{
+            System.out.println("Sa Jappo'ka Aih");
+        }
+    }
+    
+}
