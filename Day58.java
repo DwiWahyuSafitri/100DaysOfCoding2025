fc23e7249b6dc2a4b32a0838e47a229339490aa5
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-04 22:15:02 +0800

    Create Day58.java

---
+++ Day58.java
@@ -0,0 +1,17 @@
+package pkg100dayscoding;
+
+public class Day58 {
+    public static void main(String[] args) {
+       int n = 5;
+       
+        for (int i = 1; i <= n ; i++) {
+            System.out.println(i);
+        }
+        
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+        
+    }
+    
+}
