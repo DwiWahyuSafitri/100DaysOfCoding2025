ec68302b1e2ec7914f63222ddde90d52227af2ef
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-06 20:33:45 +0800

    Day60.java
    Implement a Java program to print odd and even numbers.

---
+++ Day60.java
@@ -0,0 +1,23 @@
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Day60 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan angka: ");
+        int n = sc.nextInt();
+        
+        for (int i = n; i >= 1; i--) {
+            if (i %2 == 1) {
+                System.out.println("Angka ganjil : " +i);
+            }
+        }
+             System.out.println("=========================");
+        for (int i = n; i >= 1; i--) {
+            if (i %2 == 0) {
+                System.out.println("Angka genap : "+i);
+            }
+        }
+      }
+    }  
