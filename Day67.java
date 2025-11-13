3d3d11d4700b82cdb20202b2278a69d77c819019
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-13 21:58:47 +0800

    Add Day67.java

---
+++ Day67.java
@@ -0,0 +1,15 @@
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Day67 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int p =in.nextInt();
+        
+        for (int i = 1; i <= p; i++) {
+            System.out.print("Semangat ");
+        }
+    }
+    
+}
