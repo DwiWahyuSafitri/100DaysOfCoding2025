dbf23fb9b4fbf541442aa35a4ac64244655cf5de
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-23 20:35:42 +0800

    Add Day77.java

---
+++ Day77.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day77 {
+    public static void main(String[] args) {
+        String sub = "berbahasa";
+        String rep ="berbahasa baku";
+        String tr = "  besok  DDP   ";
+        
+        System.out.println(sub.substring(5));
+        System.out.println(rep.replace("baku", "baik"));
+        System.out.println(tr.trim());
+    }
+    
+}
