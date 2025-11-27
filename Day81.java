e62b1b16a93511c7f99738561aadb6a0955413ea
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-27 22:19:54 +0800

    Add Day81.java

---
+++ Day81.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding;
+
+public class Day81 {
+    public static void main(String[] args) {
+        String [] warna = {"biru","kuning","hijau","merah","pink"};
+        
+        System.out.println(warna[0]);
+        System.out.println(warna[4]);
+    }
+    
+}
