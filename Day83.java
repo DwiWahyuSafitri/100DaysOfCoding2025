e3889f6b401be0cfa07333175d8c148950874268
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-11-29 20:46:16 +0800

    Add Day83.java

---
+++ Day83.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day83 {
+    public static void main(String[] args) {
+        int [] a ={1,2,3,4,5};
+        int hasil = 0;
+        
+        for (int i = 0; i < a.length; i++) {
+            hasil += a[i];
+        }
+        System.out.println("Hasil penjumlahann : "+hasil);
+    }
+    
+}
