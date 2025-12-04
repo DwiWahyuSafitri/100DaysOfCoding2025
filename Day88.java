8ba912385642b89f3370f0272e8a123d380871e5
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-12-04 22:24:30 +0800

    Add Day88.java

---
+++ Day88.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding;
+
+public class Day88 {
+    public static void main(String[] args) {
+        String [] warna ={"biru","pink","hijau","merah"};
+        
+        for(String color : warna){
+        System.out.println(color);
+    }
+        
+    }
+    
+}
