eea6b83cbda3b72febc647e1ca2926351c118626
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-12-06 22:42:12 +0800

    Add Day90.java

---
+++ Day90.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding;
+
+public class Day90 {
+   static String s (String nama){
+    return nama;
+}
+    public static void main(String[] args) {
+        String y = s("fitri");
+        System.out.println("halo nama saya "+y);
+    }
+    
+}
