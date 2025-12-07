66cd0ca1ace20160c744ddbcbb2a430f662028fe
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-12-07 21:27:34 +0800

    Add Day91.java

---
+++ Day91.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding;
+
+public class Day91 {
+   static void data (String nama,int umur){
+       System.out.println("Halo nama saya " +nama +" Berumur "+ umur +" tahun");
+   }
+    public static void main(String[] args) {
+        data ("Fitri",19);
+    }
+}
