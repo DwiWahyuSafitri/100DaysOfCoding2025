e7b6c9c3898cdc73367fac6df906d2b83d6289ad
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-12-09 21:24:39 +0800

    Add Day93.java
    Implement Day93 class with Math operations

---
+++ Day93.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding;
+
+public class Day93 {
+    public static void main(String[] args) {
+        double a =4.4; 
+        double b =6.7; 
+        double c = 8.9;
+        
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.floor(b));
+        System.out.println(Math.round(c));
+    }
+    
+}
