33bb051d12c96a823dbe1c62e74aa1c602590c16
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-12-10 21:34:25 +0800

    Add Day94.java

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding;
+
+public class Day94 {
+static void kata (){
+    System.out.println("Semangat Terus!!");
+}    
+    public static void main(String[] args) {
+        kata();
+    }
+}
