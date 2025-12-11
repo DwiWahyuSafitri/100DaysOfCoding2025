c79a27185b95d654dc2af2a27cae54325f70d727
DwiWahyuSafitri <dwiwahyusafitri600@gmail.com>
2025-12-11 22:51:35 +0800

    Add Day95.java

---
+++ Day95.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding;
+
+public class Day95 {
+static void biodata(String nama,int umur){
+    System.out.println("Nama saya : "+nama);
+    System.out.println("Umur saya : "+umur+" tahun");
+}    
+    public static void main(String[] args) {
+        biodata ("fitri",19);
+    }
+}
